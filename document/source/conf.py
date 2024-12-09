9e3f635d9e3901132f65ef17ec84766cd0d80ebb
AzureAD\神野創佑 <s.kanno@offgrid.co.jp>
2024-12-09 17:26:34 +0900

    修正

--- document/source/conf.py
+++ document/source/conf.py
@@ -6,6 +6,17 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+# sys.path.insert(0, os.path.abspath("../../"))
+# sys.path.insert(0, os.path.abspath("../"))
+# sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../../app"))
+# sys.path.insert(0, os.path.abspath("../../utils"))
+# sys.path.insert(0, os.path.abspath("../../route"))
+# sys.path.insert(0, os.path.abspath("../../tables"))
+
 project = 'ActionTest'
 copyright = '2024, Me'
 author = 'Me'
