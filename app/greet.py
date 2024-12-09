cd15e9693f91547abaca5aff4f1675bd150a021e
AzureAD\神野創佑 <s.kanno@offgrid.co.jp>
2024-12-09 16:51:50 +0900

    GitHubActionsテスト

---
+++ app/greet.py
@@ -0,0 +1,13 @@
+def greeting(name):
+    '''
+    greeting説明
+    Args:
+        name (str): 引数の文字
+
+    Returns:
+        str: "Hello,"+ name
+    '''
+    print("Hello,", name)
+
+
+greeting("World")
